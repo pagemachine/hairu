baf631d8e3365835783d7b4d94db31a79f20113d
Mathias Brodala <mbrodala@pagemachine.de>
2014-10-31 12:58:32 +0100

    [TASK] Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Hairu
+
+Flexible login/logout form based on Extbase/Fluid to replace the *felogin* extension shipped with TYPO3 CMS.
+
+入る (*hairu*, Japanese) means "enter"
---
+++ ext_emconf.php
@@ -0,0 +1,23 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array(
+  'title' => 'Hairu',
+  'description' => 'Flexible login/logout form based on Extbase/Fluid',
+  'category' => 'fe',
+  'author' => 'Mathias Brodala',
+  'author_email' => 'mbrodala@pagemachine.de',
+  'author_company' => 'PAGEmachine AG',
+  'state' => 'alpha',
+  'version' => '0.0.9',
+  'constraints' => array(
+    'depends' => array(
+      'typo3' => '6.2.0-6.2.99',
+    ),
+    'conflicts' => array(
+    ),
+    'suggests' => array(
+    ),
+  ),
+);
+
+?>
