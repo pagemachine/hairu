efbdea41af6ecd87fb9b50d26420a4b7e7751314
Mathias Brodala <mbrodala@pagemachine.de>
2015-07-10 09:02:57 +0200

    [TASK] Move plugin registration to TCA overrides
    This is where this belongs since in the end the tt_content TCA
    is modified which can be cached just fine.

---
+++ Configuration/TCA/Overrides/tt_content.php
@@ -0,0 +1,8 @@
+<?php
+defined('TYPO3_MODE') or die();
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+  'PAGEmachine.Hairu',
+  'Auth',
+  'LLL:EXT:hairu/Resources/Private/Language/locallang_db.xlf:plugin.auth'
+);
--- ext_tables.php
+++ ext_tables.php
@@ -1,12 +1,6 @@
 <?php
 defined('TYPO3_MODE') or die();
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-  'PAGEmachine.Hairu',
-  'Auth',
-  'LLL:EXT:hairu/Resources/Private/Language/locallang_db.xlf:plugin.auth'
-);
-
 // Static template
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
   'hairu',
