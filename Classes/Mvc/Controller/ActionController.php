5a9aa14aefa716cd5aeb87288d996fe671f9ad82
Mathias Brodala <mbrodala@pagemachine.de>
2015-08-05 14:54:49 +0200

    [TASK] Add LICENSE.txt and use short docblock

--- Classes/Mvc/Controller/ActionController.php
+++ Classes/Mvc/Controller/ActionController.php
@@ -1,29 +1,16 @@
 <?php
 namespace PAGEmachine\Hairu\Mvc\Controller;
 
-/***************************************************************
- *  Copyright notice
+/*
+ * This file is part of the PAGEmachine Hairu project.
  *
- *  (c) 2014 Mathias Brodala <mbrodala@pagemachine.de>, PAGEmachine AG
- *  
- *  All rights reserved
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 3
+ * of the License, or any later version.
  *
- *  This script is part of the TYPO3 project. The TYPO3 project is
- *  free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  The GNU General Public License can be found at
- *  http://www.gnu.org/copyleft/gpl.html.
- *
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  This copyright notice MUST APPEAR in all copies of the script!
- ***************************************************************/
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ */
 
 use TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface;
 
@@ -51,12 +38,12 @@ class ActionController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionControlle
 
     // Dynamically add argument validators
     foreach ($actionArgumentValidation as $argumentName => $validators) {
-      
+
       try {
-        
+
         $argumentValidator = $this->arguments->getArgument($argumentName)->getValidator();
       } catch (\TYPO3\CMS\Extbase\Mvc\Exception\NoSuchArgumentException $e) {
-        
+
         continue;
       }
 
@@ -65,7 +52,7 @@ class ActionController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionControlle
       foreach ($validators as $validatorConfiguration) {
 
         if (isset($validatorConfiguration['type'])) {
-        
+
           $validatorType = $validatorConfiguration['type'];
           $validatorOptions = isset($validatorConfiguration['options']) ? $validatorConfiguration['options'] : array();
           $validator = $this->validatorResolver->createValidator($validatorType, $validatorOptions);
