4c080f7f1538d26e69a282b03c403f8d1c19ab7a
Mathias Brodala <mbrodala@pagemachine.de>
2015-12-21 09:39:18 +0100

    [TASK] Declare compatibility with TYPO3 7 LTS

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = array(
   'version' => '1.3.4',
   'constraints' => array(
     'depends' => array(
-      'typo3' => '6.2.0-7.5.99',
+      'typo3' => '6.2.0-7.6.99',
     ),
     'conflicts' => array(
     ),
