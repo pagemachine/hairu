dffd57abcaeaad88ebcc8074e88ec94b3e8d8c47
Mathias Brodala <mbrodala@pagemachine.de>
2016-07-13 16:25:56 +0200

    [TASK] Drop last usage of $_EXTKEY
    This variable is bound to disappear in the future.
    
    [skip ci]

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,6 +1,6 @@
 <?php
 
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF['hairu'] = array(
   'title' => 'Hairu',
   'description' => 'Flexible login/logout form based on Extbase/Fluid',
   'category' => 'fe',
