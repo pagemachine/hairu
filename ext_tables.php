834414df9d1950b8015be3cf4ea443ada0fdc614
Mathias Brodala <mbrodala@pagemachine.de>
2017-08-14 11:49:25 +0200

    [TASK] Move static template to TCA overrides

--- ext_tables.php
+++ ext_tables.php
@@ -1,13 +1,6 @@
 <?php
 defined('TYPO3_MODE') or die();
 
-// Static template
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-    'hairu',
-    'Configuration/TypoScript',
-    'Hairu'
-);
-
 // PageTS extensions
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
     '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:hairu/Configuration/PageTS/main.ts">'
