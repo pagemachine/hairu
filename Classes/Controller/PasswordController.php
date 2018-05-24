c012d80897122542f5a50f561467a737c5071ed3
Mathias Brodala <mbrodala@pagemachine.de>
2018-05-24 10:50:46 +0200

    [BUGFIX] Fix password update with RSA encryption
    Since the password and password repeat field are RSA encrypted
    separately their encrypted values cannot be validated as being
    equal.
    
    Fix this by decrypting the encrypted password values before
    processing the update action.
    
    Fixes #33

--- Classes/Controller/PasswordController.php
+++ Classes/Controller/PasswordController.php
@@ -13,6 +13,8 @@ namespace PAGEmachine\Hairu\Controller;
  */
 
 use TYPO3\CMS\Core\Messaging\FlashMessage;
+use TYPO3\CMS\Extbase\Mvc\Exception\NoSuchArgumentException;
+use TYPO3\CMS\Rsaauth\RsaEncryptionDecoder;
 use TYPO3\CMS\Rsaauth\RsaEncryptionEncoder;
 
 /**
@@ -29,10 +31,7 @@ class PasswordController extends AbstractController
     {
         if (class_exists(RsaEncryptionEncoder::class)) {
             $rsaEncryptionEncoder = $this->objectManager->get(RsaEncryptionEncoder::class);
-
-            if ($rsaEncryptionEncoder->isAvailable()) {
-                $rsaEncryptionEncoder->enableRsaEncryption();
-            }
+            $rsaEncryptionEncoder->enableRsaEncryption();
         }
 
         if ($this->authenticationService->isUserAuthenticated()) {
@@ -51,6 +50,17 @@ class PasswordController extends AbstractController
      */
     protected function initializeUpdatePasswordAction()
     {
+        if (class_exists(RsaEncryptionDecoder::class)) {
+            $rsaEncryptionDecoder = $this->objectManager->get(RsaEncryptionDecoder::class);
+
+            try {
+                $this->request->setArgument('password', $rsaEncryptionDecoder->decrypt($this->request->getArgument('password')));
+                $this->request->setArgument('passwordRepeat', $rsaEncryptionDecoder->decrypt($this->request->getArgument('passwordRepeat')));
+            } catch (NoSuchArgumentException $e) {
+                /* Invalid states are handled by validators */
+            }
+        }
+
         // Password repeat validation needs to be added manually here to access the password value
         $passwordRepeatArgumentValidator = $this->arguments->getArgument('passwordRepeat')->getValidator();
         $passwordsEqualValidator = $this->validatorResolver->createValidator('PAGEmachine.Hairu:EqualValidator', [
