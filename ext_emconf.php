0960740f1843b43843819178770b314a1a384f95
Mathias Brodala <mbrodala@pagemachine.de>
2018-11-06 11:01:01 +0100

    [TASK] Allow installation with TYPO3v9

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '2.1.3',
     'constraints' => [
         'depends' => [
-            'typo3' => '7.6.0-8.7.99',
+            'typo3' => '7.6.0-9.5.99',
         ],
     ],
 ];
