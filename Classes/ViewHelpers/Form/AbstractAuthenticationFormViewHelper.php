b94e94d8d1651caae557f123c5db1b4acfab2a77
Mathias Brodala <mbrodala@pagemachine.de>
2018-11-06 11:24:59 +0100

    [BUGFIX] Fix form viewhelper render() signature
    These must be exactly like AbstractTagBasedViewHelper::render(), thus
    properly register all arguments.

--- Classes/ViewHelpers/Form/AbstractAuthenticationFormViewHelper.php
+++ Classes/ViewHelpers/Form/AbstractAuthenticationFormViewHelper.php
@@ -27,9 +27,27 @@ abstract class AbstractAuthenticationFormViewHelper extends AbstractTagBasedView
      */
     public function initializeArguments()
     {
+        parent::initializeArguments();
+
+        $this->registerArgument('pageUid', 'int', 'Target page uid');
+        $this->registerArgument('object', 'mixed', 'Object to use for the form. Use in conjunction with the "property" attribute on the sub tags');
+        $this->registerArgument('pageType', 'int', 'Target page type', false, 0);
+        $this->registerArgument('noCache', 'bool', 'set this to disable caching for the target page. You should not need this.', false, false);
+        $this->registerArgument('noCacheHash', 'bool', 'set this to suppress the cHash query parameter created by TypoLink. You should not need this.', false, false);
+        $this->registerArgument('section', 'string', 'The anchor to be added to the action URI (only active if $actionUri is not set)', false, '');
+        $this->registerArgument('format', 'string', 'The requested format (e.g. ".html") of the target page (only active if $actionUri is not set)', false, '');
+        $this->registerArgument('additionalParams', 'array', 'additional action URI query parameters that won\'t be prefixed like $arguments (overrule $arguments) (only active if $actionUri is not set)', false, []);
+        $this->registerArgument('absolute', 'bool', 'If set, an absolute action URI is rendered (only active if $actionUri is not set)', false, false);
+        $this->registerArgument('addQueryString', 'bool', 'If set, the current query parameters will be kept in the action URI (only active if $actionUri is not set)', false, false);
+        $this->registerArgument('argumentsToBeExcludedFromQueryString', 'array', 'arguments to be removed from the action URI. Only active if $addQueryString = TRUE and $actionUri is not set', false, []);
+        $this->registerArgument('addQueryStringMethod', 'string', 'Method to use when keeping query parameters (GET or POST, only active if $actionUri is not set', false, 'GET');
+        $this->registerArgument('fieldNamePrefix', 'string', 'Prefix that will be added to all field names within this form. If not set the prefix will be tx_yourExtension_plugin');
+        $this->registerArgument('actionUri', 'string', 'can be used to overwrite the "action" attribute of the form tag');
+
         $this->registerTagAttribute('enctype', 'string', 'MIME type with which the form is submitted');
         $this->registerTagAttribute('method', 'string', 'Transfer type (GET or POST)');
         $this->registerTagAttribute('name', 'string', 'Name of form');
+
         $this->registerUniversalTagAttributes();
     }
 
